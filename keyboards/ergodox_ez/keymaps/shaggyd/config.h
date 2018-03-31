a5d6de9d8bcbacd07c53350601ffc0ca2f5884f1
Dustin Chadwick <me@dchadwick.com>
2018-03-31 10:50:30 -0600

    Adding my QMK for the Ergodox EZ

---
+++ keyboards/ergodox_ez/keymaps/shaggyd/config.h
@@ -0,0 +1,22 @@
+#include "../../config.h"
+
+// #define ONESHOT_TAP_TOGGLE 2
+// #define ONESHOT_TIMEOUT 1000
+
+// #undef LEADER_TIMEOUT
+// #define LEADER_TIMEOUT 1000
+
+// #undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+// #undef TAPPING_TOGGLE
+// #define TAPPING_TOGGLE 2
+
+// #endif
+
+
+//define modifiers
+#define MODS_SHIFT_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+#define MODS_CTRL_MASK  (MOD_BIT(KC_LCTL)|MOD_BIT(KC_RCTRL))
+#define MODS_ALT_MASK  (MOD_BIT(KC_LALT)|MOD_BIT(KC_RALT))
+#define MODS_GUI_MASK  (MOD_BIT(KC_LGUI)|MOD_BIT(KC_RGUI))
